b092a690b36bae54f9dceac6fd7bb062a8c3b5f3
Janaki Ballav Rath <janaki@Janakis-MacBook-Air.local>
2025-10-26 14:32:09 +0530

    Completed AppDev Module 0 — All 4 assignments

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Screen 1"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondScreen()),
+            );
+          },
+          child: const Text("Go to Screen 2"),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Screen 2"),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text("This is Screen 2!", style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
